15f6aaea94e1040588437db957ce5591dc6e659a
Qining Lu <qining@stripe.com>
2025-05-29 01:11:44 -0700

    Revert "do not filter in explorer"
    This reverts commit 9939716e823be903346cfb36d6bad5dfd267f422.

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,9 +38,7 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer({
-      filterFn: undefined, // apply no filter function, every file and folder will visible
-    }),
+    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
@@ -64,9 +62,7 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer({
-      filterFn: undefined, // apply no filter function, every file and folder will visible
-    }),
+    Component.Explorer(),
   ],
   right: [],
 }
