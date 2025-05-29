9939716e823be903346cfb36d6bad5dfd267f422
Qining Lu <qining@stripe.com>
2025-05-29 01:07:26 -0700

    do not filter in explorer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -38,7 +38,9 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      filterFn: undefined, // apply no filter function, every file and folder will visible
+    }),
   ],
   right: [
     Component.Graph(),
@@ -62,7 +64,9 @@ export const defaultListPageLayout: PageLayout = {
         { Component: Component.Darkmode() },
       ],
     }),
-    Component.Explorer(),
+    Component.Explorer({
+      filterFn: undefined, // apply no filter function, every file and folder will visible
+    }),
   ],
   right: [],
 }
